04521fce6f71b85f58f6462522ca27386fa984a2
Lyudmila Lebedeva <lyudmleb@yandex.ru>
2021-01-15 17:09:45 +0700

    Рефакторинг хедера

--- src/scripts/components/Header.js
+++ src/scripts/components/Header.js
@@ -1,32 +1,44 @@
 import BaseComponent from './BaseComponent';
 
 class Header extends BaseComponent {
-  constructor(elements, authForm, handlers) {
+  constructor(elements, authForm, popup, handlers) {
     super(handlers);
     this.elements = elements;
     this.authForm = authForm;
+    this.popup = popup;
+    this.isLoggedIn = false;
 
     this.loginHandler = () => {
-      this.authForm.open();
+      if (this.isLoggedIn) {
+        this.render({ isLoggedIn: false });
+        this.isLoggedIn = false;
+      } else {
+        this.authForm.successFunc = (res) => {
+          this.isLoggedIn = true;
+          this.render({ isLoggedIn: true, userName: res.name });
+          this.popup.clearContent();
+          this.popup.close();
+        };
+        this.popup.clearContent();
+        this.popup.setContent(authForm.formElement);
+        this.popup.open();
+      }
     };
 
-    this.logoutHandler = () => {
-      // eslint-disable-next-line no-undef
-      localStorage.removeItem('token');
-      this.render({ isLoggedIn: false });
+    this.menuButtonHandler = () => {
+      this.elements.headerElement.classList.toggle('header_menu-opened');
     };
+
+    this.elements.menuButton.addEventListener('click', this.menuButtonHandler);
+    this.elements.authButton.addEventListener('click', this.loginHandler.bind(this));
   }
 
   render(props) {
     if (props.isLoggedIn) {
-      this.elements.authButton.removeEventListener('click', this.loginHandler);
       this.elements.authButton.textContent = props.userName;
-      this.elements.authButton.addEventListener('click', this.logoutHandler);
       this.elements.savedLink.classList.remove('hidden');
     } else {
-      this.elements.authButton.removeEventListener('click', this.logoutHandler);
       this.elements.authButton.textContent = 'Авторизоваться';
-      this.elements.authButton.addEventListener('click', this.loginHandler);
       this.elements.savedLink.classList.add('hidden');
     }
   }
