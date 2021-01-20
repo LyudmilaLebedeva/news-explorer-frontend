14379cddf3722c3528d3c779800da4f54594e167
Lyudmila Lebedeva <lyudmleb@yandex.ru>
2021-01-20 14:52:47 +0700

    автоматический расчет даты

--- src/scripts/api/NewsApi.js
+++ src/scripts/api/NewsApi.js
@@ -4,8 +4,10 @@ class NewsApi {
   }
 
   getNews(keyword) {
+    const fromDateTimestamp = new Date(Date.now() - (24 * 3600 * 1000 * 7));
+    const fromDate = `${fromDateTimestamp.getFullYear()}-${fromDateTimestamp.getMonth() + 1}-${fromDateTimestamp.getDate()}`;
     return fetch(
-      `http://newsapi.org/v2/everything?q=${keyword}&from=2021-01-01&pageSize=10&apiKey=${this.apiKey}`,
+      `http://newsapi.org/v2/everything?q=${keyword}&from=${fromDate}&pageSize=10&apiKey=${this.apiKey}`,
     )
       .then((res) => {
         if (res.ok) {
